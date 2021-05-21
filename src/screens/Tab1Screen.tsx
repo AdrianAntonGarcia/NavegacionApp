58232d0af54b40531f7ed2d3e6495c0b040c64cf
adriton <adrtler@gmail.com>
2021-05-21 17:42:39 +0200

    Personalizando el BottomTabNavigator

--- src/screens/Tab1Screen.tsx
+++ src/screens/Tab1Screen.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Text, View} from 'react-native';
 
 export const Tab1Screen = () => {
+  useEffect(() => {
+    console.log('Tab1ScreenEffect');
+  }, []);
   return (
     <View>
       <Text>Tab1Screen</Text>
