6be259a6ba7351950456a8270209b0025ffd7ba0
adriton <adrtler@gmail.com>
2021-05-24 12:15:49 +0200

    Instalando iconos en la app

--- src/screens/Tab1Screen.tsx
+++ src/screens/Tab1Screen.tsx
@@ -1,13 +1,18 @@
 import React, {useEffect} from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
 import {Text, View} from 'react-native';
+import {globalStyles} from '../theme/appTheme';
 
 export const Tab1Screen = () => {
   useEffect(() => {
     console.log('Tab1ScreenEffect');
   }, []);
   return (
-    <View>
-      <Text>Tab1Screen</Text>
+    <View style={globalStyles.globalMargin}>
+      <Text style={globalStyles.title}>Iconos</Text>
+      <Text>
+        <Icon name="airplane-outline" size={80} color="#900" />
+      </Text>
     </View>
   );
 };
