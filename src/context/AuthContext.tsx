ef85c93c803ed50ea0b397343aacddecfc4ddb17
adriton <adrtler@gmail.com>
2021-05-28 17:30:45 +0200

    Creando context de la app

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,30 @@
+import React, {createContext} from 'react';
+
+// Definir que información tendré aquí
+export interface AuthState {
+  isLoggedIn: boolean;
+  userName?: string;
+  favoriteIcon?: string;
+}
+
+// Estado inicial
+export const authInitialState: AuthState = {isLoggedIn: false};
+
+// Lo usuaremos para decirle a React como luce y que expone el context
+export interface AuthContextProps {
+  authState: AuthState;
+  signIn: () => void;
+}
+
+// Crear el contexto
+export const AuthContext = createContext({} as AuthContextProps);
+
+// Exponer el proveedor de información. Componente proveedor del estado
+export const AuthProvider = ({children}: any) => {
+  return (
+    <AuthContext.Provider
+      value={{authState: authInitialState, signIn: () => {}}}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
