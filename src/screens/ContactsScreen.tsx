f198ce7176de4a29c44e824e8076d96ff8a20cba
adriton <adrtler@gmail.com>
2021-06-20 19:51:56 +0200

    Disparando el sigIn desde otra pantalla

--- src/screens/ContactsScreen.tsx
+++ src/screens/ContactsScreen.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useContext} from 'react';
+import {Text, View, Button} from 'react-native';
+import {globalStyles} from '../theme/appTheme';
+import {AuthContext} from '../context/AuthContext';
 
 export const ContactsScreen = () => {
+  const {
+    signIn,
+    authState: {isLoggedIn},
+  } = useContext(AuthContext);
   return (
-    <View>
-      <Text>ContactsScreen</Text>
+    <View style={globalStyles.globalMargin}>
+      <Text style={globalStyles.title}>ContactsScreen</Text>
+      {!isLoggedIn ? (
+        <Button title="SignIn" onPress={signIn} />
+      ) : (
+        <Text>está logueado</Text>
+      )}
     </View>
   );
 };
